4def5a3d67e3f72871306b2c412b917248079596
janardhanc <janardhanc2006@gmail.com>
2020-09-14 13:28:46 -0500

    testimonials slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,16 +242,17 @@
   $(window).on('load', function() {
     aos_init();
   });
-
   $('#carousel-example').on('slide.bs.carousel', function (e) {
+
     /*
-        CC 2.0 License Iatek LLC 2018 - Attribution required
+        CC 2.0 License Iatek LLC 2018
+        Attribution required
     */
     var $e = $(e.relatedTarget);
     var idx = $e.index();
     var itemsPerSlide = 5;
     var totalItems = $('.carousel-item').length;
- 
+    
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
